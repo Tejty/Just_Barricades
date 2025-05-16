558715b705745e1d30a05ff323e30561f8f201e9
Tejty <165927081+Tejty@users.noreply.github.com>
2025-05-16 13:17:02 +0200

    Barricade block

--- src/main/java/net/tejty/just_barricades/block/ModBlocks.java
+++ src/main/java/net/tejty/just_barricades/block/ModBlocks.java
@@ -1,4 +1,42 @@
 package net.tejty.just_barricades.block;
 
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.SoundType;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.material.MapColor;
+import net.minecraft.world.level.material.PushReaction;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+import net.tejty.just_barricades.JustBarricades;
+import net.tejty.just_barricades.block.custom.BarricadeBlock;
+import net.tejty.just_barricades.item.ModItems;
+
+import java.util.function.Supplier;
+
 public class ModBlocks {
+    public static final DeferredRegister<Block> BLOCKS =
+            DeferredRegister.create(ForgeRegistries.BLOCKS, JustBarricades.MODID);
+
+
+    public static final RegistryObject<Block> OAK_BARRICADE = registerBlock("oak_barricade",
+            () -> new BarricadeBlock(BlockBehaviour.Properties.of().destroyTime(5).isViewBlocking((state, getter, pos) -> false).mapColor(MapColor.WOOD).pushReaction(PushReaction.DESTROY).sound(SoundType.WOOD).strength(5)));
+
+
+    private static <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T> block) {
+        RegistryObject<T> toReturn = BLOCKS.register(name, block);
+        registerBlockItem(name, toReturn);
+        return toReturn;
+    }
+
+    private static <T extends Block> RegistryObject<Item> registerBlockItem(String name, RegistryObject<T> block) {
+        return ModItems.ITEMS.register(name, () -> new BlockItem(block.get(), new Item.Properties()));
+    }
+
+    public static void register(IEventBus eventBus) {
+        BLOCKS.register(eventBus);
+    }
 }
--- src/main/java/net/tejty/just_barricades/block/custom/BarricadeBlock.java
+++ src/main/java/net/tejty/just_barricades/block/custom/BarricadeBlock.java
@@ -1,4 +1,33 @@
 package net.tejty.just_barricades.block.custom;
 
-public class BarricadeBlock {
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.item.context.BlockPlaceContext;
+import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.HorizontalDirectionalBlock;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.VoxelShape;
+
+public class BarricadeBlock extends HorizontalDirectionalBlock {
+    //protected static final VoxelShape  = Block.box(0.0D, 0.0D, 0.0D, 16.0D, 2.0D, 16.0D);
+
+    public BarricadeBlock(Properties pProperties) {
+        super(pProperties);
+    }
+
+    public BlockState getStateForPlacement(BlockPlaceContext pContext) {
+        return this.defaultBlockState().setValue(FACING, pContext.getHorizontalDirection().getOpposite());
+    }
+
+    @Override
+    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> pBuilder) {
+        pBuilder.add(FACING);
+    }
+
+    @Override
+    public VoxelShape getShape(BlockState pState, BlockGetter pLevel, BlockPos pPos, CollisionContext pContext) {
+        return super.getShape(pState, pLevel, pPos, pContext);
+    }
 }
